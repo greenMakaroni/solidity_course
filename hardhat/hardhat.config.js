f60e344e7407cb2efd046b06633fc62de771f090
greenMakaroni <mrtn.dawid@gmail.com>
2023-03-13 20:46:49 +0000

    deploying contract to local ganache network

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+const ganache_url = process.env.GANACHE_URL;
+const pk = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    ganache: {
+      url: ganache_url,
+      accounts: [pk],
+      chainId: 1337
+    }
+  },
   solidity: "0.8.18",
 };
