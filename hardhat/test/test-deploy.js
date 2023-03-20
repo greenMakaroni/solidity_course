f0f69104b0980856e4d591c17eec61389b0f0863
greenMakaroni <mrtn.dawid@gmail.com>
2023-03-20 13:30:27 +0000

    writing tests for simple storage contract using chai

---
+++ hardhat/test/test-deploy.js
@@ -0,0 +1,34 @@
+const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
+
+describe("SimpleStorage", () => {
+
+  let SimpleStorageFactory, simpleStorage;
+
+  beforeEach( async () => {
+    SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+    simpleStorage = await SimpleStorageFactory.deploy()
+  })
+
+  it("Deployed simple storage contract should start with favorite number equal to 0", async () => {
+    const currentValue = await simpleStorage.retrieve()
+    const expectedValue = "0"
+    // assert or expect
+
+    assert.equal(currentValue.toString(), expectedValue)
+  })
+
+  it.only("favorite number should update when we call store", async () => {
+    await simpleStorage.store("699")
+
+    const updatedValue = await simpleStorage.retrieve()
+    const expectedValue = "699"
+
+    assert.equal(updatedValue.toString(), expectedValue)
+  })
+})
+
+// we can run specific test by using
+// npx hardhat test --grep favorite number should update when we call store
+
+// or using it.only
